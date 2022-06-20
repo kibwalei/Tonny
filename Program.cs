0460a54173ed8d742bd12013022ae72fd89371a1
unknown <kibwaleianthony@gmail.com>
2022-06-20 19:31:51 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int x  = 100;
+int y = 0;
+int result;
+
+result = x/y;
+
+try{
+    result= x/y ;
+    Console.WriteLine("the result is : {0}" , result);
+
+}
+catch {
+    Console.WriteLine("Error");
+}
